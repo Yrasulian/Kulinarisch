9469c87855c59de952e5be235227b389861f6154
hasanhasanzadeh <hasan.hasanzadeh.dev@gmail.com>
2025-07-15 01:55:05 +0330

    changed me

---
+++ login.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>Home</title>
+
+    <link rel="stylesheet" href="static/css/bootstrap.min.css">
+    <link rel="stylesheet" href="static/css/all.min.css">
+
+</head>
+<body>
+
+<div class="container">
+    <div class="row">
+       <div class="col-12 col-sm-6 col-md-4 mx-auto my-5">
+           <div class="card shadow">
+               <div class="card-header">
+                   <h1 class="text-center p-1">
+                       Login
+                   </h1>
+               </div>
+               <div class="card-body">
+                   <form action="">
+                       <div class="form-group mt-1">
+                           <label for="email" class="form-label">email</label>
+                           <input type="text" class="form-control" id="email" name="email" placeholder="Enter Your Email">
+                       </div>
+                       <div class="form-group mt-1">
+                           <label for="password" class="form-label">password</label>
+                           <input type="password" class="form-control" id="password" name="password" placeholder="Enter Your Email">
+                       </div>
+                       <div class="form-group mt-2">
+                           <button type="submit" class="btn btn-primary">
+                               <i class="fa fa-sign-in"></i>
+                               <span class="mx-2">Login</span>
+                           </button>
+                       </div>
+                   </form>
+               </div>
+           </div>
+       </div>
+    </div>
+
+</div>
+
+<script src="static/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
